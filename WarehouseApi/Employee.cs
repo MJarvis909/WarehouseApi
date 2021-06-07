83fb9d814af3ea10e9fbed1e2cdd7a6effee0d74
Maks <maxek13@interia.pl>
2021-06-07 16:57:01 +0200

    first commit

---
+++ WarehouseApi/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WarehouseApi
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime EmployedAt { get; set; }
+
+    }
+}
